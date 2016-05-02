93e255444942aebee4d14bb530de2a15cbe0208b
Ryan Newton <rrnewton@gmail.com>
2016-05-02 17:41:38 -0400

    Add a shell.nix back for dev purposes

---
+++ shell.nix
@@ -0,0 +1,24 @@
+with (import <nixpkgs> {});
+
+stdenv.mkDerivation {
+
+  name = "myEnv";
+
+  buildInputs = [
+    git
+    hpx
+    stack
+    openssh
+    pkgconfig
+    bash
+    which
+    haskell.packages.lts-5_9.ghc
+  ];
+
+  STACK_IN_NIX_EXTRA_ARGS
+      = " --extra-lib-dirs=${glpk}/lib"
+      + " --extra-include-dirs=${glpk}/include"
+      + " --extra-lib-dirs=${pcre}/lib"
+      + " --extra-include-dirs=${pcre}/include"
+  ;
+}
