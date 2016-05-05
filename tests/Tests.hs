3982cb824630455c6c09928591c983f283df777b
Ryan Scott <ryan.gl.scott@gmail.com>
2016-05-05 15:05:50 -0400

    Why is fibonacci so slow

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -2,12 +2,16 @@ module Main (main) where
 
 import Foreign.HPX
 
--- import qualified Fibonacci
+import qualified Fibonacci
 import qualified HelloWorld
 
+actions :: [ActionSpec]
+actions = HelloWorld.actions
+       ++ Fibonacci.actions
+
 main :: IO ()
-main = withHPXs ({- Fibonacci.actions ++ -} HelloWorld.actions)
+main = withHPXs actions
     [ HelloWorld.main
     , HelloWorld.main
-    -- , Fibonacci.actions
+    , Fibonacci.main
     ]
